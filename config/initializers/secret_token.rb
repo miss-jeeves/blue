fb6114e5179cb52378bb38022aa16dc7c81eb4cd
Ann <miss.jeeves@gmail.com>
2011-02-27 20:05:16 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blue::Application.config.secret_token = '9fb915f29ffa26f1108d936a363d8c14029a6ba9b0fa99f1426d7b8e0396c43b89685ee252b105b97be1c39f7fbc17762b50535656757e2a2721143c1e167ee5'
