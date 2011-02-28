32556d1db7e8392969415cbac50ae463f2fc4d1c
Ann <miss.jeeves@gmail.com>
2011-02-28 15:49:28 -0500

    User model with passwords and show page ready

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+Factory.define :user do |user|
+  user.name                  "Michael Hartl"
+  user.email                 "mhartl@example.com"
+  user.island                "North America"
+  user.password              "foobar"
+  user.password_confirmation "foobar"
+end
