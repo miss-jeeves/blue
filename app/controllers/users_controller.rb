4fb0c0a9e0e3b54d17a411ce05b3fa190e5b37d3
Ann <miss.jeeves@gmail.com>
2011-02-28 23:40:37 -0500

    done with all of chapter 10

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,11 @@ class UsersController < ApplicationController
    before_filter :correct_user, :only => [:edit, :update]
    before_filter :admin_user,   :only => :destroy
    
+   def index
+     @title = "All users"
+     @users = User.paginate(:page => params[:page])
+   end
+   
 
   def show
     @user = User.find(params[:id])
